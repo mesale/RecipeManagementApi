d1f3960943c7d95c7ac2c7c273cc13afcdc6f56a
mesale <mesalemtsegaye@gmail.com>
2026-02-15 22:11:08 +0300

    initializing the pioject with the models and migrations

---
+++ recipes/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.conf import settings
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    CATEGORY_CHOICES = [
+        ('dessert', 'Dessert'),
+        ('main', 'Main Course'),
+        ('breakfast', 'Breakfast'),
+        ('vegetarian', 'Vegetarian'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructions = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    preparation_time = models.IntegerField()
+    cooking_time = models.IntegerField()
+    servings = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.title
